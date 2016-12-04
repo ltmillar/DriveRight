59786e4c86cdaa9d8b09db704faa2e40e2dc1f70
umichyp <yuping@umich.edu>
2016-12-04 18:10:52 -0500

    Create spi.h

---
+++ spi.h
@@ -0,0 +1,11 @@
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_spi.h"
+
+void SPI_Transfer(void);
+void LedTest(void);
+void init_SPI(void);
+void SPI3_ReadWriteByte(u8 TxData);
+
+
