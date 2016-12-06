84ed138d535c70e011d078079d6c612ee35b45ec
Leif Millar <ltmillar@umich.edu>
2016-12-05 23:03:53 -0500

    Played around more with stream buffer, experimenting with serial, fixed video conversion script

--- DRPi/Scripts/dashCam.py
+++ DRPi/Scripts/dashCam.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import picamera
-from time import sleep
+from time import sleep, localtime, strftime
+import subprocess
 
 def captureVideo(i):
     if i % 3 == 0:
@@ -10,17 +11,23 @@ def captureVideo(i):
 
 i = 0
 with picamera.PiCamera() as camera:
-    stream = picamera.PiCameraCircularIO(camera, seconds=10)
+    stream = picamera.PiCameraCircularIO(camera, seconds=120)
     camera.start_recording(stream, format='h264')
     camera.start_preview()
     try:
-        while i < 10:
+        while i < 4:
             camera.annotate_text = 'in loop: %d' % i
             print('in loop')
             print(i)
-            camera.wait_recording(5)
+            camera.wait_recording(10)
             if captureVideo(i):
-                stream.copy_to('/media/pi/VIDEOSD/crash/%d.h264' % i, seconds=15)
+                time = strftime("%d%b%Y_%H_%M_%S", localtime())
+                camera.annotate_text = 'Crash detected!! @ ' + time
+                camera.wait_recording(20)
+                stream.copy_to('/media/pi/VIDEOSD/crash/%s.h264' % time, seconds=60)
+                print time
+                out = subprocess.call(["./vidConv.sh", time])
+                print out
                 stream.clear()
                 camera.split_recording(stream)
             i = i+1
--- DRPi/Scripts/vidConv.sh
+++ DRPi/Scripts/vidConv.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 MP4Box -add /media/pi/VIDEOSD/crash/$1.h264 /media/pi/VIDEOSD/crash/$1.mp4
 rm /media/pi/VIDEOSD/crash/$1.h264
